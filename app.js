eccc0bf1a97d9f7d36ab3de61ee9438f0565b6b2
Matt <boblikesbbq@gmail.com>
2021-07-22 23:15:55 -0700

    never imported path module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const { removeWhiteSpaceFighters } = require("./fighterFakedata");
 const Fighters = require('./models/fighters');
 const async = require("async")
+const path = require("path");
 mongoose.connect(process.env.DB_HOST_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
